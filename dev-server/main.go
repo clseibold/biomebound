16bf59c86b4ec36d0c489e546e1093999840052b
Christian Lee Seibold <christian.seibold32@outlook.com>
2025-05-09 12:04:57 -0500

    Add a dev server for easier development and debugging.

---
+++ dev-server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"time"
+
+	"gitlab.com/clseibold/biomebound"
+	sis "gitlab.com/sis-suite/smallnetinformationservices"
+)
+
+func main() {
+	context, _ := sis.InitConfiglessMode()
+
+	// Check if "dev.pem" exists
+	certPath := "dev.pem"
+	if _, err := os.Stat(certPath); err == nil {
+
+	} else if os.IsNotExist(err) {
+		// Generate new certificate and save it as "dev.pem"
+		var pemBuffer bytes.Buffer
+		sis.GenerateSelfSignedCertificateAndPrivateKey(sis.KeyType_ED25519, "localhost", []string{"localhost"}, "", time.Now().UTC().Add(time.Hour*24*365*200), &pemBuffer)
+		os.WriteFile(certPath, pemBuffer.Bytes(), 0644)
+	} else {
+		fmt.Printf("Error checking file: %v\n", err)
+	}
+
+	hosts := []sis.HostConfig{
+		{BindAddress: "localhost", BindPort: "7000", Hostname: "localhost", Port: "7000", Upload: false, CertPath: certPath, SCGI: false},
+		{BindAddress: "localhost", BindPort: "7000", Hostname: "localhost", Port: "7000", Upload: true, CertPath: certPath, SCGI: false},
+	}
+	geminiServer, _ := context.CreateServer(sis.ServerType_Gemini, "biomebound-dev", "en", hosts...)
+
+	gameContext := biomebound.NewContext()
+	gameContext.Start()
+	gameContext.Attach(geminiServer)
+
+	context.Start()
+}
